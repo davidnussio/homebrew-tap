53186d0157d971ea5193072a7e20c9d9064450ce
David Nussio <david.nussio@gmail.com>
2026-03-23 16:37:48 +0100

    fix: brewfile error and improve it

--- Formula/envsec.rb
+++ Formula/envsec.rb
@@ -4,18 +4,18 @@ class Envsec < Formula
   url "https://registry.npmjs.org/envsec/-/envsec-1.0.0-beta.0.tgz"
   # To update the sha256, run:
   #   curl -sL https://registry.npmjs.org/envsec/-/envsec-1.0.0-beta.0.tgz | shasum -a 256
-  sha256 "PLACEHOLDER_RUN_COMMAND_ABOVE_TO_GET_SHA256"
+  sha256 "dd2c7ae3fc7dd9c3c700610d759e3dbd0048a1b8f725e288c906557790e3839e"
   license "MIT"
   version "1.0.0-beta.0"
 
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/envsec --version")
   end
-end:
+end
