1f362f1edc2131791eb5e859f0d3c9513914778d
David Nussio <david.nussio@gmail.com>
2026-03-25 20:45:14 +0100

    envsec: v1.0.0-beta.2

--- Formula/envsec.rb
+++ Formula/envsec.rb
@@ -1,12 +1,12 @@
 class Envsec < Formula
   desc "Secure environment secrets management using native OS credential stores"
   homepage "https://github.com/davidnussio/envsec"
-  url "https://registry.npmjs.org/envsec/-/envsec-1.0.0-beta.1.tgz"
+  url "https://registry.npmjs.org/envsec/-/envsec-1.0.0-beta.2.tgz"
   # To update the sha256, run:
-  #   curl -sL https://registry.npmjs.org/envsec/-/envsec-1.0.0-beta.1.tgz | shasum -a 256
-  sha256 "db07d4acd64136f5a465e80982347b8253a98e0b7ad53ad89dfef9732c1bbdb6"
+  #   
+  sha256 "31ffb457787f16ba0fbc8e4107844f5aa0692b926808866cfd2035851a818e85"
   license "MIT"
-  version "1.0.0-beta.1"
+  version "1.0.0-beta.2"
 
   depends_on "node"
 
